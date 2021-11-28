88d1d3ca14f44a6fd1bb453f93251e70e760b243
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-28 13:31:30 -0300

    criando linha de tabuleiro

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,10 @@
-import Subdivisao from '../components/Subdivisao'
+import Linha from '../components/Linha'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Subdivisao preta/>
-      <Subdivisao/>
-      <Subdivisao preta/>
+    <div>
+      <Linha />
     </div>
   )
 }
